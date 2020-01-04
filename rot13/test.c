e9c123fc9ee0ee5af90bdf177f8c15a9383eeefe
Dan Rachou <dan.rachou@gmail.com>
2020-01-04 20:46:31 +0100

    Passing new tests

--- rot13/test.c
+++ rot13/test.c
@@ -1,10 +1,40 @@
 #include <criterion/criterion.h>
 #include "rot.h"
 
-Test (empty, four_letter_string)
+Test (none, only_lowercase_letters)
 {
   char *s1 = rot13("test");
   char *s2 = "grfg";
 
   cr_assert_str_eq(s1, s2, "Actual '%s' expected '%s'", s1, s2);
+}
+
+Test (none, only_uppercase_letters)
+{
+
+  char *s1 = rot13("TEST");
+  char *s2 = "GRFG";
+
+  cr_assert_str_eq(s1, s2, "Actual '%s' expected '%s'", s1, s2);
+}
+
+Test (none, special_characters)
+{
+  char *s1 = rot13("+=_-/");
+  char *s2 = "+=_-/";
+
+  cr_assert_str_eq(s1, s2, "Actual '%s' expected '%s'", s1, s2);
+}
+
+Test (none, only_numbers)
+{
+  char *s1 = rot13("12345");
+  char *s2 = "12345";
+
+  cr_assert_str_eq(s1, s2, "Actual '%s' expected '%s'", s1, s2);
+}
+
+Test (none, empty_string)
+{
+  cr_assert_str_eq(rot13(""), "");
 }
