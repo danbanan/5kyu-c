36f928772afcac2150b85029c25da2d6ba1c6522
Dan Rachou <dan.rachou@gmail.com>
2020-01-05 21:03:18 +0100

    Final solution

---
+++ maximum-subarray-sum/maxsequence.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int maxSequence(const int *array, size_t n)
+{
+  int max = 0;
+  int tmp = 0;
+
+  for(int i=0; i<n; i++)
+    {
+      int x = *(array + i);
+      tmp = (tmp + x > 0) ? tmp + x : 0;
+      max = (tmp > max) ? tmp : max;
+    }
+
+  return (max < 0) ? 0 : max;
+}
